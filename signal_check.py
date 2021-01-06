8ff47690048cbf83e62db91b048835175d573032
smlee75 <smlee75@linecorp.com>
2021-01-06 14:44:02 +0900

    Update signal_check.py

--- signal_check.py
+++ signal_check.py
@@ -20,27 +20,38 @@ order by date
 """
 vix = pd.read_sql(qry, engine_dev, parse_dates=['date'], index_col=['date'])
 
-spx.ret = spx.close.pct_change()
-spx.intra_vol = (spx.high - spx.low) / spx.close
-spx.intra_bar = spx.intra_vol.rolling(3).mean().shift(1)*100*0.5
+qry = """
+SELECT date, open, close FROM marketdata.index_data p
+left join marketdata.index_info i on i.index = p.index_index
+where i.index_name = 'SKEW'
+order by date
+"""
+skew = pd.read_sql(qry, engine_dev, parse_dates=['date'], index_col=['date'])
 
-vix.mov_2 = vix.close.rolling(window=2).mean()
-vix.mov_20 = vix.close.rolling(window=20).mean()
-skew.mov_20 = skew.close.rolling(window=20).mean()
-vix.mov_long = vix.close.rolling(window=100).mean()
-vix.mov_short = vix.close.rolling(window=5).mean()
+spx['ret'] = spx.close.pct_change()
+spx['intra_vol'] = (spx.high - spx.low) / spx.close
+spx['intra_bar'] = spx.intra_vol.rolling(3).mean().shift(1)*100*0.5
+
+vix['mov_2'] = vix.close.rolling(window=2).mean()
+vix['mov_20'] = vix.close.rolling(window=20).mean()
+vix['mov_long'] = vix.close.rolling(window=100).mean()
+vix['mov_short'] = vix.close.rolling(window=5).mean()
+skew['mov_20'] = skew.close.rolling(window=20).mean()
 
 nn = 500
-vix.rank = vix.mov_20.dropna().rolling(window=nn).apply(lambda x:( x.rank(ascending=True)[-1])/nn,raw=False)
-skew.rank = skew.mov_20.dropna().rolling(window=nn).apply(lambda x:( x.rank(ascending=True)[-1])/nn,raw=False)
+vix['rank'] = vix.mov_20.dropna().rolling(window=nn).apply(lambda x:( x.rank(ascending=True)[-1])/nn,raw=False)
+skew['rank'] = skew.mov_20.dropna().rolling(window=nn).apply(lambda x:( x.rank(ascending=True)[-1])/nn,raw=False)
 
-vix.trend = np.where(vix.mov_long > vix.mov_short, 1, 0)
-vix.signal = pd.concat([vix.rank, skew.rank], axis=1).dropna().apply(lambda x: vix_s(x[0],x[1]),axis=1)
+vix['trend'] = np.where(vix.mov_long > vix.mov_short, 1, 0)
+vix['signal'] = pd.concat([vix.rank, skew.rank], axis=1).dropna().apply(lambda x: vix_s(x[0],x[1]),axis=1)
 
 rt_signal = vix[['signal', 'trend']].dropna().apply(lambda x: rt_s(x[0],x[1]),axis=1)
+scale_n = 100
 vol_scale = vix.mov_2.dropna().rolling(window=scale_n).apply(lambda x:( x.rank(ascending=True)[-1])/scale_n,raw=False)
 
-print(pd.concat([rt_signal.shift(1), spx.intra_bar, vol_scale.shift(1)], axis=1))
+ot = pd.concat([rt_signal.shift(1), spx.intra_bar, vol_scale.shift(1)], axis=1)
+ot.columns = ['vix_signal', 'intra_barrier', 'exposure']
+print(ot)
 
 
 def rt_s(r,t):
